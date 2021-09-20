a0d1d9fb28612ddd632461914946f0e4135425b8
Hajin Kim <rhrnak624@naver.com>
2021-09-21 00:43:16 +0900

    initial commit

--- app/src/main/java/com/example/mirimtalk_template/MainActivity.java
+++ app/src/main/java/com/example/mirimtalk_template/MainActivity.java
@@ -3,12 +3,86 @@ package com.example.mirimtalk_template;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.POST;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtSearch;
+    Button btnSearch;
 
+    TextView userName, userLocation, userBio, userPublicRepos, userPublicGists, userFollowers, userFollowing;
+    ImageView userImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtSearch = findViewById(R.id.edtSearch);
+        btnSearch = findViewById(R.id.btnSearch);
+
+        userName = findViewById(R.id.userName);
+        userLocation = findViewById(R.id.userLocation);
+        userImage = findViewById(R.id.userImage);
+        userBio = findViewById(R.id.userBio);
+        userPublicRepos = findViewById(R.id.userPublicRepos);
+        userPublicGists = findViewById(R.id.userPublicGists);
+        userFollowers = findViewById(R.id.userFollowers);
+        userFollowing = findViewById(R.id.userFollowing);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        final JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Call<User> call = jsonPlaceHolderApi.getPosts(edtSearch.getText().toString());
+
+                call.enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        if(response.isSuccessful()) {
+                            // Log.d("Test", "성공");
+                            User user = response.body();
+                            userName.setText(user.getName());
+                            userLocation.setText(user.getLocation());
+                            Glide.with(getApplicationContext()).load(user.getAvatar_url()).into(userImage);
+                            userBio.setText(user.getBio());
+                            userPublicRepos.setText(Integer.toString(user.getPublic_repos()));
+                            userPublicGists.setText(Integer.toString(user.getPublic_gists()));
+                            userFollowers.setText(Integer.toString(user.getFollowers()));
+                            userFollowing.setText(Integer.toString(user.getFollowing()));
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+                        t.printStackTrace();
+                    }
+                });
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/mirimtalk_template/User.java
@@ -0,0 +1,83 @@
+package com.example.mirimtalk_template;
+
+public class User {
+    private String name;
+
+    private String location;
+
+    private String avatar_url;
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    private String bio;
+
+    private int public_repos;
+
+    private int public_gists;
+
+    private int followers;
+
+    private int following;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public int getPublic_repos() {
+        return public_repos;
+    }
+
+    public void setPublic_repos(int public_repos) {
+        this.public_repos = public_repos;
+    }
+
+    public int getPublic_gists() {
+        return public_gists;
+    }
+
+    public void setPublic_gists(int public_gists) {
+        this.public_gists = public_gists;
+    }
+
+    public int getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(int followers) {
+        this.followers = followers;
+    }
+
+    public int getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(int following) {
+        this.following = following;
+    }
+}
